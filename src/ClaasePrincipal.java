d40b7df993ee62d329ed7df56af2a3413a002b10
MelEliseGomes <melelisegomesc@gmail.com>
2025-03-11 12:25:03 -0300

    Comando de print

---
+++ src/ClaasePrincipal.java
@@ -0,0 +1,23 @@
+
+public class ClaasePrincipal {
+
+	public static void main(String[] args) {
+
+		// Printar mensagem pulando linha
+		System.out.println("Pulando linha"); //sysout + space
+		
+		// Printar mensagem sem pular linha
+		System.out.printf("Sem pular linha"); //sysout + space"
+		
+		System.out.println("Pulando linha"); //sysout + space
+		
+		//Sequência de scape
+		//Usar aspas duplas
+		System.out.println("Meu nome é \"Fulano\"");
+		//Usar aspas simples
+		System.out.println("Meu nome é \'Fulano\'");
+		//Pular linha, quebrar linha
+		System.out.println("Meu nome é \n Fulano");
+	}
+
+}
